9ec1e788751e98e1e5cdb804093b8257d8a5a7c0
Jan Andrei Reyes <167843080+LibrarianDarchivo@users.noreply.github.com>
2025-06-02 18:15:03 +0800

    Add files via upload

---
+++ Student Score Calculator/Score_Calc.php
@@ -0,0 +1,21 @@
+<?php
+function calculateAvg($scores)
+{
+    return array_sum($scores) / count($scores);
+}
+
+$name = $_POST['name'];
+$subjects = $_POST['subject'];
+$scores = $_POST['score'];
+
+$avg = calculateAvg($scores);
+
+// Output
+echo "<h2>Student Report</h2>";
+echo "Name: " . htmlspecialchars($name) . "<br><br>";
+
+for ($i = 0; $i < count($subjects); $i++) {
+    echo "Subject: " . htmlspecialchars($subjects[$i]) . " - Score: " . htmlspecialchars($scores[$i]) . "<br>";
+}
+
+echo "<br><strong>Average Score:</strong> " . round($avg, 2);
